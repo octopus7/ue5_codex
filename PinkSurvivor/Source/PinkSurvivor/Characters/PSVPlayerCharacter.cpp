9f4185b6a3e1c9d3d5a43b4b84a214b0bdba25d1
octopus7 <octopus7net@gmail.com>
2025-10-09 16:12:54 +0900

    이동 및 카메라

---
+++ PinkSurvivor/Source/PinkSurvivor/Characters/PSVPlayerCharacter.cpp
@@ -0,0 +1,111 @@
+#include "Characters/PSVPlayerCharacter.h"
+
+#include "Camera/CameraComponent.h"
+#include "EnhancedInputComponent.h"
+#include "EnhancedInputSubsystems.h"
+#include "Engine/LocalPlayer.h"
+#include "GameFramework/CharacterMovementComponent.h"
+#include "GameFramework/PlayerController.h"
+#include "GameFramework/SpringArmComponent.h"
+#include "InputActionValue.h"
+
+APSVPlayerCharacter::APSVPlayerCharacter()
+{
+    PrimaryActorTick.bCanEverTick = false;
+
+    CameraBoom = CreateDefaultSubobject<USpringArmComponent>(TEXT("CameraBoom"));
+    CameraBoom->SetupAttachment(RootComponent);
+    CameraBoom->bUsePawnControlRotation = false;
+    CameraBoom->bInheritPitch = false;
+    CameraBoom->bInheritYaw = false;
+    CameraBoom->bInheritRoll = false;
+    CameraBoom->bDoCollisionTest = true;
+
+    FollowCamera = CreateDefaultSubobject<UCameraComponent>(TEXT("FollowCamera"));
+    FollowCamera->SetupAttachment(CameraBoom, USpringArmComponent::SocketName);
+    FollowCamera->bUsePawnControlRotation = false;
+
+    bUseControllerRotationPitch = false;
+    bUseControllerRotationYaw = false;
+    bUseControllerRotationRoll = false;
+
+    if (UCharacterMovementComponent* Movement = GetCharacterMovement())
+    {
+        Movement->bOrientRotationToMovement = true;
+        Movement->RotationRate = FRotator(0.f, 720.f, 0.f);
+        Movement->MaxWalkSpeed = 600.f;
+        Movement->BrakingDecelerationWalking = 2048.f;
+        Movement->bUseControllerDesiredRotation = false;
+    }
+}
+
+void APSVPlayerCharacter::PostInitializeComponents()
+{
+    Super::PostInitializeComponents();
+
+    if (CameraBoom)
+    {
+        CameraBoom->TargetArmLength = CameraBoomLength;
+        CameraBoom->SetRelativeRotation(CameraRelativeRotation);
+    }
+}
+
+void APSVPlayerCharacter::BeginPlay()
+{
+    Super::BeginPlay();
+
+    if (AController* Controller = GetController())
+    {
+        if (APlayerController* PlayerController = Cast<APlayerController>(Controller))
+        {
+            if (ULocalPlayer* LocalPlayer = PlayerController->GetLocalPlayer())
+            {
+                if (UEnhancedInputLocalPlayerSubsystem* Subsystem = LocalPlayer->GetSubsystem<UEnhancedInputLocalPlayerSubsystem>())
+                {
+                    if (DefaultInputMapping)
+                    {
+                        Subsystem->AddMappingContext(DefaultInputMapping, 0);
+                    }
+                }
+            }
+
+            const FRotator CameraYawOnly(0.f, CameraRelativeRotation.Yaw, 0.f);
+            PlayerController->SetControlRotation(CameraYawOnly);
+        }
+    }
+}
+
+void APSVPlayerCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+    Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+    if (UEnhancedInputComponent* EnhancedInputComponent = Cast<UEnhancedInputComponent>(PlayerInputComponent))
+    {
+        if (MoveAction)
+        {
+            EnhancedInputComponent->BindAction(MoveAction, ETriggerEvent::Triggered, this, &APSVPlayerCharacter::Move);
+            EnhancedInputComponent->BindAction(MoveAction, ETriggerEvent::Completed, this, &APSVPlayerCharacter::Move);
+        }
+    }
+}
+
+void APSVPlayerCharacter::Move(const FInputActionValue& Value)
+{
+    if (!Controller || !CameraBoom)
+    {
+        return;
+    }
+
+    const FVector2D MovementVector = Value.Get<FVector2D>();
+    if (MovementVector.IsNearlyZero())
+    {
+        return;
+    }
+
+    const FRotator CameraYawOnly(0.f, CameraBoom->GetComponentRotation().Yaw, 0.f);
+    const FVector ForwardDirection = FRotationMatrix(CameraYawOnly).GetUnitAxis(EAxis::X);
+    const FVector RightDirection = FRotationMatrix(CameraYawOnly).GetUnitAxis(EAxis::Y);
+
+    AddMovementInput(ForwardDirection, MovementVector.Y);
+    AddMovementInput(RightDirection, MovementVector.X);
+}
---
+++ PinkSurvivor/Source/PinkSurvivor/Characters/PSVPlayerCharacter.h
@@ -0,0 +1,45 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "PSVPlayerCharacter.generated.h"
+
+class USpringArmComponent;
+class UCameraComponent;
+class UInputMappingContext;
+class UInputAction;
+struct FInputActionValue;
+
+UCLASS()
+class PINKSURVIVOR_API APSVPlayerCharacter : public ACharacter
+{
+    GENERATED_BODY()
+
+public:
+    APSVPlayerCharacter();
+
+    virtual void PostInitializeComponents() override;
+    virtual void BeginPlay() override;
+    virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+protected:
+    void Move(const FInputActionValue& Value);
+
+    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category="Components")
+    TObjectPtr<USpringArmComponent> CameraBoom;
+
+    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category="Components")
+    TObjectPtr<UCameraComponent> FollowCamera;
+
+    UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category="Input")
+    TObjectPtr<UInputMappingContext> DefaultInputMapping;
+
+    UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category="Input")
+    TObjectPtr<UInputAction> MoveAction;
+
+    UPROPERTY(EditAnywhere, BlueprintReadOnly, Category="Camera", meta=(ClampMin="0.0"))
+    float CameraBoomLength = 800.0f;
+
+    UPROPERTY(EditAnywhere, BlueprintReadOnly, Category="Camera")
+    FRotator CameraRelativeRotation = FRotator(-50.0f, -45.0f, 0.0f);
+};
