a214dc7a1d70e538e00674605dc8bdbd6a8fb457
octopus7 <octopus7net@gmail.com>
2026-03-14 22:47:51 +0900

    5.7.4용 코덱스 작업용 빈프로젝트

---
+++ CodexInven/Source/CodexInvenEditor.Target.cs
@@ -0,0 +1,15 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+using UnrealBuildTool;
+using System.Collections.Generic;
+
+public class CodexInvenEditorTarget : TargetRules
+{
+	public CodexInvenEditorTarget( TargetInfo Target) : base(Target)
+	{
+		Type = TargetType.Editor;
+		DefaultBuildSettings = BuildSettingsVersion.V6;
+		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_7;
+		ExtraModuleNames.Add("CodexInven");
+	}
+}
