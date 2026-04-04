55b2236a7cf591da71b46736a6eb87a5ec09d172
octopus7 <octopus7net@gmail.com>
2026-04-04 18:55:22 +0900

    미로 프로젝트 추가

---
+++ CodexMazeWalker/Source/CodexMazeWalker/CodexMazeWalker.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "CodexMazeWalker.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, CodexMazeWalker, "CodexMazeWalker" );
---
+++ CodexMazeWalker/Source/CodexMazeWalkerEditor.Target.cs
@@ -0,0 +1,15 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+using UnrealBuildTool;
+using System.Collections.Generic;
+
+public class CodexMazeWalkerEditorTarget : TargetRules
+{
+	public CodexMazeWalkerEditorTarget( TargetInfo Target) : base(Target)
+	{
+		Type = TargetType.Editor;
+		DefaultBuildSettings = BuildSettingsVersion.V6;
+		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_7;
+		ExtraModuleNames.Add("CodexMazeWalker");
+	}
+}
