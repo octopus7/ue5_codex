ff10f47cc1c39cb1859f23601f5fac6570fd8223
octopus7 <octopus7net@gmail.com>
2026-04-11 00:53:56 +0900

    UMG용 실습 프로젝트

---
+++ CodexUMG/Source/CodexUMG/CodexUMG.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "CodexUMG.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, CodexUMG, "CodexUMG" );
